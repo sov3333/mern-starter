9e04a2f8f747cdfc4deb4645bfdbe031cceb090e
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-07 09:03:07 +0800

    create api endpoint

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from Express!" });
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
