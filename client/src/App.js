e18051ef5f6e85ac658476c151064de3ff6c5382
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-07 13:57:08 +0800

    create routes, refactor category list to component

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import millify from 'millify';
-
-const categoryArray = [
-  'gaming',
-  'metaverse',
-  'play-to-earn',
-  'guild-scholarship',
-  'gambling',
-  // 'decentralized-perpetuals',
-  // 'yield-farming',
-  // 'yield-aggregator',
-  // 'prediction-markets',
-  // 'privacy-coins',
-  // 'zero-knowledge-zk',
-  // 'artificial-intelligence',
-];
+import CategoryDetails from './components/CategoryDetails';
+import Home from './components/Home';
+import Categories from './components/Categories';
 
 function App() {
   const [data, setData] = useState(null);
-  const [dataCGCoinsCategories, setDataCGCoinsCategories] = useState(null);
 
   useEffect(() => {
     fetch("/api")
     .then((res) => res.json())
     .then((data) => setData(data.message));
-
-    // call coingecko /coins/categories api endpoint
-    async function fetchCGCoinsCategoriesData() {
-      const response = await axios.get('/coingecko/coins/categories');
-
-      // filter only data that match our categoryArray
-      let tempArray = [];
-      response.data.map((e, i) => {
-        for (let i=0; i<categoryArray.length; i++) {
-          if (e.id === categoryArray[i]) {
-            tempArray.push(e)
-            console.log(e)
-          };
-        }
-      })
-      // set the filtered data in state
-      setDataCGCoinsCategories(tempArray);
-    }
-    fetchCGCoinsCategoriesData();
-
   }, [])
 
   return (
@@ -54,31 +20,17 @@ function App() {
         <h2>
           {!data ? "Loading..." : data}
         </h2>
-        <div>
-          <h3>Categories</h3>
-          <div>
-            {!dataCGCoinsCategories ? "Loading..." : (
-              dataCGCoinsCategories.map((e, i) => (
-                <div className="category" key={i}>
-                  <div>
-                    <h4>{e.name}</h4>
-                    <div>
-                      {e.top_3_coins.map((src, i) => (
-                        <img src={src} key={i} />
-                      ))}
-                    </div>
-                  </div>
-                  <ul>
-                    <li>Market Cap: ${millify(e.market_cap)}</li>
-                    <li>24h Volume: ${millify(e.volume_24h)}</li>
-                    <li>24h Price Change: {millify(e.market_cap_change_24h)}%</li>
-                  </ul>
-                </div>
-              ))
-            )}
-          </div>
-        </div>
       </header>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/category" element={<Categories />} />
+          <Route path="/category/:id" element={<CategoryDetails />} />
+        </Routes>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
     </div>
   );
 }
---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+
+const categoryArray = [
+  'gaming',
+  'metaverse',
+  'play-to-earn',
+  'guild-scholarship',
+  'gambling',
+  // 'decentralized-perpetuals',
+  // 'yield-farming',
+  // 'yield-aggregator',
+  // 'prediction-markets',
+  // 'privacy-coins',
+  // 'zero-knowledge-zk',
+  // 'artificial-intelligence',
+];
+
+const Categories = () => {
+  const [dataCGCoinsCategories, setDataCGCoinsCategories] = useState(null);
+
+  useEffect(() => {
+    // call coingecko /coins/categories api endpoint
+    async function fetchCGCoinsCategoriesData() {
+      const response = await axios.get('/coingecko/coins/categories');
+
+      // filter only data that match our categoryArray
+      let tempArray = [];
+      response.data.map((e, i) => {
+        for (let i=0; i<categoryArray.length; i++) {
+          if (e.id === categoryArray[i]) {
+            tempArray.push(e)
+            console.log(e)
+          };
+        }
+      })
+      // set the filtered data in state
+      setDataCGCoinsCategories(tempArray);
+    }
+    fetchCGCoinsCategoriesData();
+
+  }, [])
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      {!dataCGCoinsCategories ? "Loading..." : (
+        dataCGCoinsCategories.map((e, i) => (
+          <div className="category" key={i}>
+            <div>
+              <Link to={`/category/${e.id}`}>
+                <h4>{e.name}</h4>
+              </Link>
+              <div>
+                {e.top_3_coins.map((src, i) => (
+                  <img src={src} key={i} />
+                ))}
+              </div>
+            </div>
+            <ul>
+              <li>Market Cap: ${millify(e.market_cap)}</li>
+              <li>24h Volume: ${millify(e.volume_24h)}</li>
+              <li>24h Price Change: {millify(e.market_cap_change_24h)}%</li>
+            </ul>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Categories
